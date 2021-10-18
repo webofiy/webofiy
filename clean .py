7388b0589827f4af542420d2d1306d7b2688fd5f
webofiy <mrwaqas356@gmail.com>
2021-10-18 14:33:01 -0700

    web basics

---
+++ clean .py
@@ -0,0 +1,4 @@
+l="<a><abbr><acronym><b><bdo><big><br><button><cite><code><dfn><em><i><img><input><kbd><label><map><object><output><q><samp><script><select><small><span><strong><sub><sup><textarea><time><tt><var>"
+k= l.replace("<" ," " ).replace('>'," ") 
+print(k)
+h2>Here are the block-level elements in HTML: </h2>a  abbr  acronym  b  bdo  big  br  button  cite  code  dfn  em  i  img  input  kbd  label  map  object  output  q  samp  script  select  small  span  strong  sub  sup  textarea  time  tt  var
